3081ab9b45304a5f1264c4fc422f0a5439ba1c15
unknown <believeinhope1@hotmail.com>
2018-12-19 19:54:28 -0500

    initial commit

---
+++ src/GetAsciiValue.java
@@ -0,0 +1,14 @@
+//Click run to see your unicode for Grand Circus
+public class GetAsciiValue {
+	public static void main(String[] args) {
+		String s = "GrandCircus";
+		for (int i = 0; i < s.length(); i++)
+			System.out.println("ASCII value of: " + s.charAt(i) + " is:" + (int) s.charAt(i));
+		
+		
+	}
+		
+		
+		
+}	
+
